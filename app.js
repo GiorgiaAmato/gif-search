1b44cad1e807478e658d4c5bf8919e447ac7d941
GiorgiaAmato <78232397+GiorgiaAmato@users.noreply.github.com>
2022-10-10 15:34:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,3 +8,15 @@ deleteTag.addEventListener('click', function(event){
 httpRequest = new XMLHttpRequest();
 
 httpRequest.open("GET", "http://api.giphy.com/v1/gifs/trending?api_key=Rpme6fWvSm44NF5kliQGmfz111RsNCZI")
+httpRequest.send();
+httpRequest.onload = function () {
+    var gifsData = JSON.parse(httpRequest.response).data;
+    gifsData.forEach(gif => {
+        var url = gif.images.downsized_medium.url;
+        var width = gif.images.downsized_medium.width;
+        var height = gif.images.downsized_medium.height;
+                console.log(width + height);
+
+    });
+
+}
